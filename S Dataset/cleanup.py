1a2f143a1fbe0e1b2ad2f8e7ad96eb13ed9cfc03
Samantha Ngo <samantha.ngo04@myhunter.cuny.edu>
2020-05-11 17:23:17 -0400

    Fixed data

--- S Dataset/cleanup.py
+++ S Dataset/cleanup.py
@@ -1,12 +1,20 @@
 from csv import reader
 import csv
+import pandas
 
+# Fix indices in stop_directory.csv:
+# df = pandas.read_csv('new.csv')
+# print(df)
+# df = df.drop(['Stop ID'], axis=1)
+# df.to_csv('stop_directory.csv', index=True)
+
+# Generate transfers_directory
 transfers_data = {}
 transfers = {}
 with open('stop_directory.csv', 'r') as read_obj:
     csv_reader = reader(read_obj)
     for row in csv_reader:
-        print(row)
+        # print(row)
         transfers[row[0]] = []
         try:
             transfers_data[row[1]+row[2]].append(row[0])
@@ -14,26 +22,27 @@ with open('stop_directory.csv', 'r') as read_obj:
             transfers_data[row[1]+row[2]] = []
             transfers_data[row[1]+row[2]].append(row[0])
 
-print("transfers_data:")
-print(transfers_data)
-print("==================================================================================")
-print("transfers:")
-print(transfers)
-print("==================================================================================")
+
+# print("transfers_data:")
+# print(transfers_data)
+# print("==================================================================================")
+# print("transfers:")
+# print(transfers)
+# print("==================================================================================")
 
 for key, value in transfers_data.items():
-    print(key, '->', value)
+    # print(key, '->', value)
     currVals = value.copy()
     removals = value.copy()
     while len(currVals) > 0:
         currVal = currVals[0]
         currVals.remove(currVal)
         removals.remove(currVal)
-        print("---", currVal, "->", removals)
+        # print("---", currVal, "->", removals)
         transfers[currVal] = removals
         removals = value.copy()
 
-print(transfers)
+# print(transfers)
 # with open('test.csv', 'a') as f:
 #     for key, value in transfers.items():
 #         retStr = key + ","
@@ -43,7 +52,7 @@ print(transfers)
 #         print(retStr)
 #         f.write(retStr)
 
-with open('test.csv', 'w', newline='') as write_obj:
+with open('stop_transfers.csv', 'w', newline='') as write_obj:
     csv_writer = csv.writer(write_obj)
     for key, value in transfers.items():
         if key == "Stop ID":
@@ -51,5 +60,5 @@ with open('test.csv', 'w', newline='') as write_obj:
         else:
             retVals = value
             retVals.insert(0, key)
-        print(retVals)
+        # print(retVals)
         csv_writer.writerow(retVals)
