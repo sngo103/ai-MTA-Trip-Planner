6a80ace840847e2a4320d847fb4fc3c70f62a08a
Daniel Rozenzaft <daniel.rozenzaft@macaulay.cuny.edu>
2020-05-18 19:41:42 -0400

    Added more test cases

--- testing.py
+++ testing.py
@@ -9,18 +9,24 @@ alphstr = 'abcdefghiklmnopqrstuvwyzABCDEFGHIKLMNOPQRSTUVWY'
 directory_data, transfers_data, stop_order_data, mta = initialize_system()
 current_state = None
 
+
+def generateRoute(start, end, mta, current_state):
+    return route(start, end, mta, current_state)
+
 for i in range(200):
     start = random.choice(alphstr)
     end = random.choice(alphstr)
-    output = route(start, end, mta, current_state)
+    output = generateRoute(start, end, mta, current_state)
     f.write('\n\n' + str(i))
     if not output:
-        print (i)
-        print(start)
-        print(end)
+        print(i)
     else:
         f.write(output)
 
+    directory_data, transfers_data, stop_order_data, mta = initialize_system()
+
 f.close()
+
+
 
 
